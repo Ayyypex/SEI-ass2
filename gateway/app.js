54a00115878f0c992211edb344e62b606cdee3d0
mattv52 <matthewvolaris@yahoo.com.au>
2023-10-29 15:40:02 +1030

    Added basic layout for gateway and added makefile to addition

--- gateway/app.js
+++ gateway/app.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
 const port = 3000;
 
-app.get('/add', (req, res) => {
-  const { num1, num2 } = req.query;
-  if (!num1 || !num2) {
-    return res.status(400).json({ error: 'Both num1 and num2 parameters are required.' });
-  }
+let minikube_ip = "192.168.49.2";
 
-  const result = parseFloat(num1) + parseFloat(num2);
-  res.json({ result });
+let microservices = {
+  addition: { url: minikube_ip+"31304", secret:"addition-secret" },
+  multipliction: { url: minikube_ip+"", secret:"mulipliction-secret" },
+  subtraction: { url: minikube_ip+"", secret:"subtraction-secret" },
+  rand_num: { url: minikube_ip+"", secret:"rand-num-secret" },
+  rand_string: { url: minikube_ip+"", secret:"rand-string-secret" },
+  timestamp: { url: minikube_ip+"", secret:"timestamp-secret" },
+}
+
+app.get("/api/add/*", function(req, res) {
+  // let microservice = "";
 });
 
-app.listen(port, () => {
-  console.log(`Addition microservice is listening on port ${port}`);
-});
+
+// app.get('/add', (req, res) => {
+//   const { num1, num2 } = req.query;
+//   if (!num1 || !num2) {
+//     return res.status(400).json({ error: 'Both num1 and num2 parameters are required.' });
+//   }
+
+//   const result = parseFloat(num1) + parseFloat(num2);
+//   res.json({ result });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Gateway is listening on port ${port}`);
+// });
