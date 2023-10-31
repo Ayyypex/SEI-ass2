5bc0dcbf6b00af27674622c732b45f83f43f446d
matthew <matthew@Ubuntu.myguest.virtualbox.org>
2023-10-31 17:17:40 +1030

    Add timestamp tests.

---
+++ tests/test_timestamp.py
@@ -0,0 +1,42 @@
+import subprocess
+import requests
+import time
+from datetime import datetime
+
+def get_ip():
+    cmd = ["minikube", "ip"]
+    return subprocess.check_output(cmd).decode('utf-8').strip('\n')
+
+def get_port():
+    cmd = ["kubectl", "get", "service", "gateway-service", '--output=jsonpath="{.spec.ports[0].nodePort}"']
+    return subprocess.check_output(cmd).decode('utf-8').strip('"')
+
+def test_request():
+    try:
+        url = 'http://' + get_ip() + ':' + get_port() + '/timestamp'
+        response = requests.get(url)
+        assert(response.status_code == 200)
+        responseString = response.content.decode('utf-8').strip("\n")
+        time = responseString.split(',')[2].split(' ')[1].lstrip('0')
+        now = str(datetime.now().strftime("%I:%M:%S")).lstrip('0')
+        assert(time == now)
+        return 0
+    except:
+        print("Timestamp Test: request failed")
+        return 1
+
+def main():
+    result = 0
+    # Run tests multiple times due to the naturally changing
+    # result of the service.
+    for _ in range(0,5):
+        result += test_request()
+        time.sleep(3)
+
+    if result == 0:
+        print("All tests passed.")
+    else:
+        print(f"{result} tests failed.")
+
+if __name__ == "__main__":
+    main()
