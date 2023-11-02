21bb565e2a5e4835c0ccf4aaf2ea7ca79e2519bf
matthew <matthew@Ubuntu.myguest.virtualbox.org>
2023-11-02 18:39:15 +1030

    Fix float regex to allow negatives and full stops. Add test_all.py

--- multiplication-microsvc/app.js
+++ multiplication-microsvc/app.js
@@ -9,7 +9,7 @@ app.get('/multiply', (req, res) => {
     return res.status(400).json({ error: 'Both num1 and num2 parameters are required.' });
   }
 
-  var regex = /^[0-9]+$/;
+  var regex = /^[-+]?[0-9]*\.?[0-9]+$/;
   if (!num1.match(regex)) {
     return res.status(400).json({ error: 'num1 must be a number' });
   }
